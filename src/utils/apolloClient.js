808501760252c07e68744756610c3d21ab42e675
Parminder Klair <perminder.klair@gmail.com>
2018-03-10 13:29:15 +0530

    added apollo auth

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,12 +1,30 @@
 import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 
 import config from './config';
 
+const httpLink = createHttpLink({
+  uri: config.graphQlUri,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: config.graphQlUri }),
-  cache: new InMemoryCache()
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  connectToDevTools: config.debug,
 });
 
 export default client;
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 export default {
+  debug: process.env.NODE_ENV === 'development',
   siteName: 'Kickoff React',
   graphQlUri: 'https://api.graph.cool/simple/v1/cj0ikxyx8zu4d013355xzxmzh',
 };
