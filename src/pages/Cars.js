d39b87a3779c2ee9804f26cb851436970aa706eb
Parminder Klair <perminder.klair@gmail.com>
2018-03-25 22:30:05 +0530

    updated graphql query

--- src/pages/Cars.js
+++ src/pages/Cars.js
@@ -1,33 +1,10 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import gql from 'graphql-tag';
-import { graphql, compose } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 
-class Cars extends React.Component {
-  render() {
-    const { data, createCar } = this.props;
-
-    if (data.loading) {
-      return <p>loading...</p>;
-    }
-
-    return (
-      <section className="section">
-        <Helmet title="About" />
-        <div className="container">
-          <h1 className="title">
-            Cars from Graph.cool
-          </h1>
-          <button className="button is-primary" onClick={() => createCar({ title: 'bmw' })}>Create car</button>
-          <ul>{data.allCars.map(car => <li key={car.id}>{car.title}</li>)}</ul>
-        </div>
-      </section>
-    );
-  }
-}
-
-const query = gql`
-  query CarsListQuery {
+const allCarsQuery = gql`
+  {
     allCars {
       id
       title
@@ -44,30 +21,57 @@ const createCarMutation = gql`
   }
 `;
 
-export default compose(
-  graphql(query),
-  graphql(createCarMutation, {
-    name: 'createCar',
-    options: {
-      update: (proxy, { data: { createCar } }) => {
-        const data = proxy.readQuery({ query });
-        data.allCars.push(createCar);
-        proxy.writeQuery({ query, data });
-      },
-    },
-    props: ({ createCar }) => ({
-      createCar: input => {
-        createCar({
-          variables: input,
-        })
-          .then(result => {
-            console.log('result', result); // eslint-disable-line
-            alert('Car created successfully!'); // eslint-disable-line
-          })
-          .catch(error => {
-            console.log('error', error); // eslint-disable-line
-          });
-      },
-    }),
-  }),
-)(Cars);
+class Cars extends React.Component {
+  render() {
+
+    const handleCarAdded = () => {
+      alert('Car created successfully!'); // eslint-disable-line
+    }
+
+    const onCarUpdate =(cache, { data: { createCar } }) => {
+      const data = cache.readQuery({ query: allCarsQuery });
+      data.allCars.push(createCar);
+      cache.writeQuery({ query: allCarsQuery, data });
+    };
+
+    return (
+      <section className="section">
+        <Helmet title="About" />
+        <div className="container">
+          <h1 className="title">
+            Cars from Graph.cool
+          </h1>
+          <Mutation
+            mutation={createCarMutation}
+            variables={{ title: 'bmw' }}
+            update={onCarUpdate}
+            onCompleted={handleCarAdded}
+          >
+            {(createCar, { loading, error, data }) => (
+              <div>
+                <button className="button is-primary" onClick={createCar}>
+                Create car
+                </button>
+                {data && <p>Car created successfully!</p>}
+                {loading && <p>Loading...</p>}
+                {error && <p>Error :( Please try again</p>}
+              </div>
+            )}
+          </Mutation>
+          <ul>
+            <Query query={allCarsQuery}>
+              {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error :(</p>;
+
+              return data.allCars.map(car => <li key={car.id}>{car.title}</li>)
+            }}
+            </Query>
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Cars;
